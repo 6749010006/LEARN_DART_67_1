9edd0cd69a66911df87fddd4b1bcbd14677609b8
mny-14 <6749010039@phuketvc.ac.th>
2025-07-26 14:03:38 +0700

    step 1; Structure

---
+++ dart1.dart
@@ -0,0 +1,24 @@
+//structurn in Dart
+void mian()
+{
+  var name = "John"; 
+  print(name);
+  print("Your name is $name.");
+  print("Your name is ${name = 'Jane'}.");
+
+  int age = 20;
+  print("Your age is $age.");
+
+ // Good variable names
+String userName = "Alice"; 
+int userAge = 12;
+double temperatureCelsius = 36.6;
+bool isActive = true;
+
+// Bad variable names
+//bool isInactive = true; // bool isActive = false;
+//String 1name = "Bob" ;
+//int max count = 50;
+//double totalPercent = 100.0;
+//Var if = "Error";// reserved keyword
+}
