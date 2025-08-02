1d31fd5e443d9bb1d63d5f7735f3188d5214cdad
mny-14 <6749010039@phuketvc.ac.th>
2025-08-02 10:42:48 +0700

    Step8: Enum

---
+++ dart9.dart
@@ -0,0 +1,16 @@
+//Function
+
+void maim() {
+  printMsg("Joel");
+  print(getMsg("Joel"));
+  print(add(10,20));
+}
+
+void printMsg(String name) {
+  print("Hello1, $name");
+}
+
+String getMsg(String name) {
+   return "Hello2, $name";
+}
+
